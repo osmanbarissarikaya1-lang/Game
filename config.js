0c95ed58d258287e04a1caae2868cbdc56161ed7
osmanbarissarikaya1-lang <osman.baris.sarikaya1@gmail.com>
2026-08-20 19:31:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
 window.SUPABASE_URL = "https://jizagbzzpofdjblbuaps.supabase.co";
 window.SUPABASE_KEY = "sb_publishable_NF-GDxjVXI7d6QRQGOQTTw_oZWKdYhA";
+const SUPER_ADMINS = ["osman.baris.sarikaya1@gmail.com"];
