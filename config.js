8e227e242471cf4b438c5308c618c13a9abd17e0
osmanbarissarikaya1-lang <osman.baris.sarikaya1@gmail.com>
2026-08-20 15:16:00 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 window.SUPABASE_URL = "https://jizagbzzpofdjblbuaps.supabase.co";
-window.SUPABASE_KEY = "sb_publishable_qporywyKwx5TRAxRdxur_A_MxGhdQha";
+window.SUPABASE_KEY = "sb_publishable_NF-GDxjVXI7d6QRQGOQTTw_oZWKdYhA";
